b21b08825fc1020fbe06f2a5f498b852685f81ea
Peter <peter@2i.tv>
2011-08-15 11:19:40 +0100

    Removed cache files from the sites/default/files directory

--- sites/default/files/civicrm/templates_c/en_US/%%93/938/938D45F8%%Subject.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-06-06 18:17:08
-         compiled from CRM/Block/Subject.tpl */ ?>
-<span class='crm_blocktitle'><?php echo $this->_tpl_vars['subject']; ?>
-</span>
--- sites/default/files/civicrm/templates_c/en_US/%%F0/F0F/F0F71C33%%version.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2011-06-06 18:17:08
-         compiled from CRM/common/version.tpl */ ?>
-3.4.2
